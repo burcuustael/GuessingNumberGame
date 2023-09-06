d96977886a0369e1988a12e3467e7ed9a04d4fda
burcuustael <burcustael@gmail.com>
2023-09-06 21:48:19 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+var heart =3;
+var random=0;
+
+function Guess(){
+
+    var number = document.getElementById("number").value;
+
+    if(heart==3){
+        random = Math.floor(Math.random()*10)+1;
+    }
+
+    if(number==random){
+        document.getElementById('result').
+
+    }
+
+}
+
+function ShowButton(id){
+
+}
+
+function HideButton(id){
+    
+}
